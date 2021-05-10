ea46a4ffd8bc576891b8bbbc0968c82ab0469781
brecert <11599528+Brecert@users.noreply.github.com>
2021-05-10 05:12:20 -0400

    intitial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const dbg = (...args) => {
+  console.log(...args);
+  return args[0];
+};
+
+document.querySelectorAll("nav > .mid").forEach(
+  (el) =>
+    // even though onpointer events are closer to what I want
+    // it will always register as down even wehn going for a drag
+    (el.onclick = () =>
+      el.scrollIntoView({
+        block: "end",
+        behavior: "smooth",
+      }))
+);
+
+window.onload = () => {
+  document.querySelector(".mid > div").textContent =
+    `mid\n` +
+    Array(100)
+      .fill(0)
+      .map((_, i) => i.toString(10).padStart(3, "0"))
+      .join("\n");
+
+  document.querySelector(".mid").scrollIntoView();
+};
